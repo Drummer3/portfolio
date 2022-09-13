ffeb950cd2bc8ca68e815bd1488bc49ad5cd1f8e
Data Munjishvili <data.munjishvili@pulsar.ai>
2022-09-13 18:49:08 +0400

    hide mail icon from top right

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import type { Component } from 'solid-js'
-import {
-	Container,
-	Box,
-	IconButton,
-	useColorMode,
-	notificationService,
-} from '@hope-ui/solid'
+import { Container, Box, IconButton, useColorMode } from '@hope-ui/solid'
 
 import Welcome from './components/Welcome'
 import FullPageTiles from './components/FullPageTiles'
 
-import { HiOutlineMail, HiOutlineMoon, HiOutlineSun } from 'solid-icons/hi'
+import { HiOutlineMoon, HiOutlineSun } from 'solid-icons/hi'
 
 const App: Component = () => {
 	const { colorMode, toggleColorMode } = useColorMode()
@@ -18,24 +12,6 @@ const App: Component = () => {
 	return (
 		<>
 			<Box position="fixed" right="$3" top="$3" zIndex={100}>
-				<IconButton
-					mx="$2"
-					onClick={() => {
-						try {
-							navigator.clipboard.writeText('munjishvili@proton.me')
-							notificationService.show({
-								status: 'success',
-								title: 'Mail copied successfully!',
-							})
-						} catch (error) {
-							notificationService.show({ status: 'warning', title: 'Error!' })
-						}
-					}}
-					variant="outline"
-					bgColor="$background"
-					aria-label="Copy Mail"
-					icon={<HiOutlineMail size={24} />}
-				/>
 				<IconButton
 					onclick={toggleColorMode}
 					variant="outline"
